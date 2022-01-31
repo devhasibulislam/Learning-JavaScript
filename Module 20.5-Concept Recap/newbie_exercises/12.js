93c217b82dc2d3ee30629f365d083233e504afe8
Hasibul Islam <hasib143sl@gmail.com>
2022-01-31 13:33:11 +0600

    basic exercises

---
+++ Module 20.5-Concept Recap/newbie_exercises/12.js
@@ -0,0 +1,24 @@
+/* Write a JS program to find sum of first and last digit of a number. */
+/* Write a JS program to find first and last digit of a number. */
+function findFirstLastDigit(number) {
+    /* approach 1 */
+    storeNumber = number.toString()
+    console.log("First digit is: " + storeNumber[0] + ' & ' + "Last digit is: " + storeNumber[storeNumber.length - 1]);
+    console.log("Summation of first and last digits: " + (parseInt(storeNumber[0]) + parseInt(storeNumber[storeNumber.length - 1])));
+
+    /* approach 2 */
+    // let reminder = 0, count = 0, firstDigit, lastDigit;
+    // while (number != 0) {
+    //     reminder = number % 10;
+    //     count++;
+    //     if (count == 1)
+    //         lastDigit = reminder;
+    //     else
+    //         firstDigit = reminder;
+    //     number = parseInt(number / 10);
+    // }
+    // console.log("First digit: " + firstDigit + ' & ' + "Last digit: " + lastDigit);
+    // console.log("Summation of first and last digits: " + (firstDigit + lastDigit));
+}
+
+findFirstLastDigit(1935205);
---
+++ Module 20.5-Concept Recap/newbie_exercises/13.js
@@ -0,0 +1,1 @@
+/* Write a JS program to swap first and last digits of a number. */
