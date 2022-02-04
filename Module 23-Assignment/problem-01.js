65234d8a9725c721de92f8b2cdaeb26647994321
Hasibul Islam <hasib143sl@gmail.com>
2022-02-04 22:12:12 +0600

    bug fix

--- Module 23-Assignment/problem-01.js
+++ Module 23-Assignment/problem-01.js
@@ -1,7 +1,6 @@
 function anaToVori(ana) {
-    const vori = ana / 16;
-    const mon = parseFloat(vori.toFixed(2));
-    return mon; /* return as number */
+    const vori = parseFloat((ana / 16).toFixed(2));
+    return vori; /* return as number */
 }
 
 // console.log(typeof(anaToVori(35))); /* check wheather it is number or string */
